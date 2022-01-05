0e4df7b19df8ddff744423b9fbb98041223b2de8
pk0220 <pratikkhamkar220@gmail.com>
2022-01-05 09:37:30 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 export default function App() {
+  return <Cdac></Cdac>;
+}
+
+// HTML :pre defined TAGS
+// REACT : user defined TAGS=>COMPONENT
+{
+  /* <div></div>
+<cdac></cdac> */
+}
+
+export function Cdac() {
   return (
     <div>
-      <div>Hello</div>
+      <h1>Hello Cdac</h1>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
+        consequuntur dignissimos numquam velit quas perferendis molestiae
+        nesciunt quidem impedit temporibus ducimus exercitationem est distinctio
+        nostrum voluptatibus, vitae possimus odio illum sit placeat minima
+        doloremque veniam error deserunt! Tenetur, eum unde?
+      </div>
     </div>
   );
 }
-
